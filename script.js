b0e8e3c46b6269a7560b2ed5094c0f4803137620
Mark-vas <vasyaev.mark@mail.ru>
2021-03-19 12:05:38 +0300

    Добавил стили и изменил код в "script.js"

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 'use strict'
-document.querySelector('button').style.border = '20px double green'
 
 const goods = [
-    { title: 'Shirt', price: 150, currency: "руб." },
+    { title: 'Shirt', price: 150, img: 'images/white-t-shirt-svgrepo-com.svg' },
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = function (title, price, currency = '$') {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price} ${currency}</p></div>`;
+const renderGoodsItem = function (title, price, img = 'images/cross-svgrepo-com.svg', currency = '$') {
+    return `<div class="goods-item"><img src=${img}><h3>${title}</h3><p>${price} ${currency}</p></div>`;
 };
 
 const renderGoodsList = function (list) {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.currency));
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.img, item.currency));
     document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 
 renderGoodsList(goods);
-
-const div = document.querySelectorAll('.goods-item')
-for (let i = 0; i < div.length; i++) {
-    div[i].style.width = '100px'
-    div[i].style.backgroundColor = 'lightgreen'
-}
-
-const h3 = document.querySelectorAll('h3')
-for (let i = 0; i < h3.length; i++) {
-    h3[i].style.color = 'red'
-    h3[i].style.fontSize = '25px'
-}
