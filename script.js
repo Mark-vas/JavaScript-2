0455e68e49411e37fda9281afefa03f17ddf3aa5
Mark-vas <vasyaev.mark@mail.ru>
2021-03-22 17:17:22 +0300

    Изменил все функции на стрелочные

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = function (title, price, img = 'images/cross-svgrepo-com.svg', currency = '$') {
+const renderGoodsItem = (title, price, img = 'images/cross-svgrepo-com.svg', currency = '$') => {
     return `<div class="goods-item"><img src=${img}><h3>${title}</h3><p>${price} ${currency}</p></div>`;
 };
 
-const renderGoodsList = function (list) {
+const renderGoodsList = (list) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.img, item.currency));
     document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
