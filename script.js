d639db227742355f1fa69694fcf991a74bc72b24
Mark-vas <vasyaev.mark@mail.ru>
2021-03-26 16:45:29 +0300

    Начал делать ДЗ к уроку 3

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 'use strict'
+function makeGETRequest(url, callback) {
+    var xhr;
+
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            callback(xhr.responseText);
+        }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class GoodsItem {
-    constructor(title, price, img = 'images/cross-svgrepo-com.svg', currency = '$') {
-        this.title = title;
+    constructor(product_name, id_product = 'Отсутвсует', price = 'По запросу', img = 'images/cross-svgrepo-com.svg', currency = '$') {
+        this.product_name = product_name;
+        this.id_product = id_product;
         this.price = price;
         this.img = img;
         this.currency = currency;
     }
+
     render() {
-        return `<div class="goods-item"><img src=${this.img}><h3>${this.title}</h3><p>${this.price} ${this.currency}</p></div>`;
+        return `<div class="goods-item"><img src=${this.img}><h3>${this.product_name}</h3><p>Код товара: ${this.id_product}<p>Цена: ${this.price} ${this.currency}</p></div>`;
     }
 }
 
@@ -15,26 +38,36 @@ class GoodsList {
     constructor() {
         this.goods = [];
     }
-    fetchGoods() {
+
+    fetchGoods(cb) {
+        makeGETRequest(`${API_URL}/catalogData.json`, (goods) => {
+            this.goods = JSON.parse(goods);
+            cb();
+        })
+    }
+
+    /*fetchGoods() {
         this.goods = [
             { title: 'Shirt', price: 150, img: 'images/white-t-shirt-svgrepo-com.svg' },
             { title: 'Socks', price: 50 },
             { title: 'Jacket', price: 350 },
             { title: 'Shoes', price: 250 },
         ];
-    }
+    }*/
+
     render() {
         let listHTML = '';
         this.goods.forEach(good => {
-            const goodItem = new GoodsItem(good.title, good.price, good.img, good.currency);
+            const goodItem = new GoodsItem(good.product_name, good.id_product, good.price, good.img, good.currency);
             listHTML += goodItem.render();
         })
         document.querySelector('.goods-list').innerHTML = listHTML;
     }
+
     sumBasket() {
         let sum = 0;
         this.goods.forEach(xxx => {
-            const sumItem = new GoodsItem(xxx.title, xxx.price, xxx.img, xxx.currency);
+            const sumItem = new GoodsItem(xxx.product_name, xxx.id_product, xxx.price, xxx.img, xxx.currency);
             sum = sum + xxx.price;
         })
         document.querySelector('.goods-list').insertAdjacentHTML("afterend", `<p class = 'sumBasket'> Итого: ${sum} $</p>`)
@@ -42,13 +75,28 @@ class GoodsList {
 }
 
 const list = new GoodsList();
-document.querySelector('button').addEventListener('click', function () {
-    list.fetchGoods();
+/*list.fetchGoods(() => {
     list.render();
     if (document.querySelector('.sumBasket')) {
 
     }
     else {
-        list.sumBasket()
+        setTimeout(() => {
+            list.sumBasket()
+        }, 3000);
     }
+});*/
+
+document.querySelector('button').addEventListener('click', function () {
+    list.fetchGoods(() => {
+        list.render();
+        if (document.querySelector('.sumBasket')) {
+
+        }
+        else {
+            setTimeout(() => {
+                list.sumBasket()
+            }, 3000);
+        }
+    })
 })
