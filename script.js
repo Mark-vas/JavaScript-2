517643b81d976e873ea901d564d55a5f5f4e00ad
Mark-vas <vasyaev.mark@mail.ru>
2021-04-09 17:51:50 +0300

    Осталось добавить "Поиск по имени"

--- script.js
+++ script.js
@@ -1,18 +1,151 @@
 'use strict'
 const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
+Vue.component('catalog-list', {
+    props: ['goods', 'goodsshow', 'carts', 'sum'],
+    template: `
+    <div class="catalog" v-show="goodsshow">
+        <h1>Каталог</h1>
+        <div class="goods-list-1">
+            <goods-item-catalog v-for="good in goods" :good="good" :goods="goods" :carts="carts" :sum="sum"></goods-item-catalog>
+        </div>
+        
+    </div>
+    
+    `,
+    // methods: {
+    //     addtocart() {
+
+    //     }
+    // }
+});
+
+Vue.component('goods-item-catalog', {
+    props: ['good', 'goods', 'carts', 'sum'],
+    template: `
+        <div class="goods-item">
+            <h3>{{ good.product_name }}</h3>
+            <p>{{ good.price }}</p>
+            <button v-on:click="addtocart($event, goods, carts, sum)" :data-id="good.id_product" :sum="sum">Добавить</button>
+           
+        </div>
+    `,
+    methods: {
+        // addtocart() {
+        //     this.$emit('click')
+        // },
+        addtocart(event, goods, carts, sum) {
+            //Добавить элемент в корзину
+            // console.log(event.target)
+            // console.log(this.good)
+            // console.log(goods)
+            // console.log(sum)
+            for (let i = 0; i < goods.length; i++) {
+                if (goods[i].id_product == event.target.getAttribute('data-id')) {
+                    carts.unshift(goods[i])
+                    //Рассчитать сумму в корзине.
+                    carts.forEach(elemcart => {
+                        sum[0] = sum[0] + elemcart.price;
+                    })
+                }
+            }
+        },
+    }
+
+});
+
+Vue.component('search-list', {
+    props: ['searchresults', 'searchshow', 'addtocart'],
+    template: `
+    <div class="search-list" v-show="searchshow">
+        <h1>Результаты поиска</h1>
+        <button v-on:click="searchhide">X</button>
+        <goods-item-search v-for="search in searchresults" :search="search" v-on:click="addtocart"></goods-item-search>
+    </div>
+    `,
+    methods: {
+        searchhide() {
+            this.$emit('click')
+        }
+    }
+})
+
+Vue.component('goods-item-search', {
+    props: ['search'],
+    template: `
+        <div class="goods-item">
+            <h3>{{ search.product_name }}</h3>
+            <p>{{ search.price }}</p>
+            <button v-on:click="addtocart"
+                :data-id="search.id_product">Добавить</button>
+        </div>
+    `,
+    methods: {
+
+    }
+});
+
+Vue.component('cart-list', {
+    props: ['carts', 'cartshow', 'sumbasket', 'sum'],
+    template: `
+    <div class="cart-list" v-show="cartshow">
+        <h1>Корзина</h1>
+        <button v-on:click="carthide">Х</button>
+        <div class="basket">
+            <goods-item-cart v-for="cart in carts" :cart="cart" :carts="carts" :sum="sum"></goods-item-cart>
+        </div >
+        <h1 class="totalsumBasket" :sum="sum" v-show="sumbasket">Итого: {{sum[0]}}</h1>
+    </div>
+    `,
+    methods: {
+        carthide() {
+            this.$emit('click')
+        },
+        // delcart() {
+
+        // }
+    }
+})
+
+Vue.component('goods-item-cart', {
+    props: ['cart', 'carts', 'sum'],
+    template: `
+    <div class="goods-item">
+        <h3>{{ cart.product_name }}</h3>
+        <p>{{ cart.price }}</p>
+        <button v-on:click="delcart($event, carts, sum)" :data-id="cart.id_product">Удалить</button>
+    </div>
+    `,
+    methods: {
+        delcart(event, carts, sum) {
+            //Удаленить элемент из корзины.
+            for (let i = 0; i < carts.length; i++) {
+                if (carts[i].id_product == event.target.getAttribute('data-id')) {
+                    carts.splice([i], 1)
+                    //Пересчитать сумму в корзине с учетом удаленных элементов
+                    sum[0] = 0
+                    carts.forEach(elemcart => {
+                        sum[0] = sum[0] + elemcart.price;
+                    })
+                    break
+                }
+            }
+        },
+    }
+})
+
 const app = new Vue({
     el: '#app',
     data: {
         goods: [],
-        GoodsShow: true,
+        goodsshow: true,
         carts: [],
-        CartShow: false,
-        sumBasket: false,
+        cartshow: false,
+        sumbasket: false,
         searchresults: [],
-        SearchShow: false,
-        valueText: '',
-        sum: 0
+        searchshow: false,
+        valuetext: '',
+        sum: [0]
     },
     methods: {
         makeGETRequest(url) {
@@ -45,54 +178,53 @@ const app = new Vue({
             })
         },
 
-        addToCart(event) {
-            //Добавить элемент в корзину
-            for (let i = 0; i < this.goods.length; i++) {
-                if (this.goods[i].id_product == event.target.getAttribute('data-id')) {
-                    this.carts.unshift(this.goods[i])
-                }
-                //Рассчитать сумму в корзине.
-                this.sum = 0
-                this.carts.forEach(elemcart => {
-                    this.sum = this.sum + elemcart.price
-                })
-            }
-        },
+        // addtocart(event) {
+        //     //Добавить элемент в корзину
+        //     console.log(event)
+        //     for (let i = 0; i < this.goods.length; i++) {
+        //         if (this.goods[i].id_product == event.target.getAttribute('data-id')) {
+        //             this.carts.unshift(this.goods[i])
+        //         }
+        //         //Рассчитать сумму в корзине.
+        //         this.sum = 0
+        //         this.carts.forEach(elemcart => {
+        //             this.sum = this.sum + elemcart.price
+        //         })
+        //     }
+        // },
 
-        cartList() {
+        cartlist() {
             //Показать корзину
-            this.GoodsShow = false
-            this.CartShow = true
-            this.SearchShow = false
-            this.sumBasket = true
+            this.goodsshow = false
+            this.cartshow = true
+            this.searchshow = false
+            this.sumbasket = true
         },
 
-        cartHide() {
+        carthide() {
             //Скрыть корзину
-            this.CartShow = false
-            this.GoodsShow = true
+            this.cartshow = false
+            this.goodsshow = true
         },
 
-        delItem(event) {
-            //Удаленить элемент из корзины. Пока не знаю как удалить все.
-            console.log(event.target.getAttribute('data-id'))
-            for (let i = 0; i < this.carts.length; i++) {
-                if (this.carts[i].id_product == event.target.getAttribute('data-id')) {
-                    this.carts.splice([i], 1)
-                    //Пересчитать сумму в корзине с учетом удаленных элементов
-                    this.sum = 0
-                    this.carts.forEach(elemcart => {
-                        this.sum = this.sum + elemcart.price
-                    })
-                    break
-                }
+        // delcart(event) {
+        //     //Удаленить элемент из корзины. Пока не знаю как удалить все.
+        //     for (let i = 0; i < this.carts.length; i++) {
+        //         if (this.carts[i].id_product == event.target.getAttribute('data-id')) {
+        //             this.carts.splice([i], 1)
+        //             //Пересчитать сумму в корзине с учетом удаленных элементов
+        //             this.sum = 0
+        //             this.carts.forEach(elemcart => {
+        //                 this.sum = this.sum + elemcart.price
+        //             })
+        //             break
+        //         }
+        //     }
+        // },
 
-            }
-        },
-
-        searchRes() {
+        searchres() {
             // Поиск продукта в каталоге
-            this.SearchShow = true
+            this.searchshow = true
             let value = document.querySelector('.navigation-search input').value
             const regexp = new RegExp(value, 'i');
             this.searchresults = this.goods.filter(search => {
@@ -100,12 +232,13 @@ const app = new Vue({
             })
         },
 
-        searchHide() {
+        searchhide() {
             //Скрыть "Результаты поиска"
-            this.SearchShow = false
+            this.searchshow = false
             this.searchresults = []
         }
     },
+
     mounted() {
         this.makeGETRequest(`${API_URL}/catalogData.json`)
             .then((data) => {
@@ -116,3 +249,21 @@ const app = new Vue({
             })
     },
 })
+
+/* <div class="goods-item">
+            <h3>{{ good.product_name }}</h3>
+            <p>{{ good.price }}</p>
+            <button v-on:click="addtocart($event)" v-bind:data-id="good.id_product">Добавить</button>
+        </div> */
+
+/* <div class="goods-item">
+            <h3>{{ search.product_name }}</h3>
+            <p>{{ search.price }}</p>
+            <button v-on:click="addtocart($event)"
+                v-bind:data-id="search.id_product">Добавить</button>
+        </div> */
+/* <div class="goods-item">
+    <h3>{{ cart.product_name }}</h3>
+    <p>{{ cart.price }}</p>
+    <button v-on:click="delcart($event)" v-bind:data-id="cart.id_product">Удалить</button>
+</div> */
