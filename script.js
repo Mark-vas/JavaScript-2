76f32c7b8d1b2016932be52df0341e67fcebc5c4
Mark-vas <vasyaev.mark@mail.ru>
2021-04-12 12:28:19 +0300

    Итог

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ const regexp1 = new RegExp('\'', 'g')
 console.log(str1.replace(regexp1, '"'))
 // 2 задание
 const str2 = "'Lorem ipsum dolor, sit amet consect'etur adipisicing elit. Maiores alias in 'ipsa ex autem modi' atque nost'rum nulla, eaque reprehenderit cumque except'uri nemo, off'iciis expedita ipsum. Perferendis incidunt mollitia amet'";
-const regexp2 = new RegExp('^\'', 'g')
-const regexp2_1 = new RegExp('\'$', 'g')
-const regexp2_2 = new RegExp('/s\'', 'g')
-const regexp2_3 = new RegExp('\'/s', 'g')
-console.log(str2.replace(regexp2, '"').replace(regexp2_1, '"').replace(regexp2_2, ' "').replace(regexp2_3, '" '))
+const regexp2 = new RegExp("^\'|\'$", "g")
+const regexp2_2 = new RegExp("\\s\'", "g")
+const regexp2_3 = new RegExp("\'\\s", "g")
+console.log(str2.replace(regexp2, '"').replace(regexp2_2, ' "').replace(regexp2_3, '" '))
